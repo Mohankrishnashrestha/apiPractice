8aebb7ee69242a6efed384a37779ca9d7de8e883
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2025-01-31 22:48:50 +0545

    1st part

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,19 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Description() {
   const params = useParams();
-
-  async function fetchindication() {
-    try {
-      const res = await fetch("https://fakestoreapi.com/products/" + params.id);
-      const d = await res.json();
-      console.log(d);
-    } catch (error) {
-      console.error(error);
-    }
+  const [change, setChange] = useState("");
+  console.log(params.id);
+  async function handle() {
+    const res = await fetch("https://fakestoreapi.com/products/" + params.id);
+    const d = await res.json();
+    console.log(d);
+    setChange(d);
   }
-  fetchindication();
-  return <div></div>;
+  useEffect(() => {
+    handle();
+  }, []);
+
+  return (
+    <div className="w-full ">
+      <div className="max-w-7xl mx-auto flex flex-col items-center justify-center p-5 border w-[400px] rounded-3xl hover:shadow-2xl scale-75 ease">
+        <img src={change.image} alt="" className="w-[300px] h-[300px]" />
+        <h3> Name:{change.title}</h3>
+        <h4>Price:{change.price}</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Description;
